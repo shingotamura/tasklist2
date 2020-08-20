925891ab46ca9dbf69745d8db1bc0a4081a9e1e8
shingo.tamura <mr.shingo-0409@outlook.jp>
2020-08-20 14:09:34 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'toppages/index'
+  root to: 'toppages#index'
   
   root to: 'tasks#index'
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end

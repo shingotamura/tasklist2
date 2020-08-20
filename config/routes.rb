82e582fa20095fce3bef334d6696d8648db73830
shingo.tamura <mr.shingo-0409@outlook.jp>
2020-08-20 16:06:38 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'toppages/index'
   root to: 'toppages#index'
   
-  root to: 'tasks#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :tasks
   
   get 'signup', to: 'users#new'
